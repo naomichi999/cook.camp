0891e3839360c1900ea497650eb953944abf535b
naomichi999 <naomichi999@gmail.com>
2019-10-16 14:09:04 +0900

    [add]ユーザー情報編集画面の作成

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -1,4 +1,5 @@
 class Curriculum < ApplicationRecord
+	attachment :curriculum_image
 	belongs_to :skill_note
 	validates :chapter_title, :section_title, :curriculum_content, presence: true
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f1e5bd549b6ed294eeab413009febc14c585ce03209b45513a531e70cf4583468de69e85c0bd0380c9b40d128ee835f8ce8b170419581b4f97bea0b4d5bdbd9e'
