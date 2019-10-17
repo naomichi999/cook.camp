511077efff2836da52c75811ba74a78241a1c5d3
naomichi999 <naomichi999@gmail.com>
2019-10-17 19:57:07 +0900

    [add]マイレシピ追加画面で動画投稿機能の作成(投稿は可能だが表示がされない)

--- app/models/my_recipe.rb
+++ app/models/my_recipe.rb
@@ -1,5 +1,7 @@
 class MyRecipe < ApplicationRecord
 	attachment :cooking_image
+	mount_uploader :cooking_video, VideoUploader
+	# 動画投稿用gemであるcarrierwave用の記述
 	has_many :comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 	belongs_to :genre
