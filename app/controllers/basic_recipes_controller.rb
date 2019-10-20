1a8b7accad338cf1d99d1b935f168d2fb4527853
naomichi999 <naomichi999@gmail.com>
2019-10-20 16:03:27 +0900

    [add]基本のレシピ(ジャンル選択画面・ジャンル別レシピ画面)の作成

--- app/controllers/basic_recipes_controller.rb
+++ app/controllers/basic_recipes_controller.rb
@@ -3,7 +3,34 @@ class BasicRecipesController < ApplicationController
   def index
   end
 
-  def genre_list
+  def meat
+    @basic = BasicRecipe.where(genre_id:1)
+    @pages = @basic.page(params[:page]).per(7)
+  end
+
+  def fish
+    @basic = BasicRecipe.where(genre_id:2)
+    @pages = @basic.page(params[:page]).per(7)
+  end
+
+  def vegetable
+    @basic = BasicRecipe.where(genre_id:3)
+    @pages = @basic.page(params[:page]).per(7)
+  end
+
+  def noodle
+    @basic = BasicRecipe.where(genre_id:4)
+    @pages = @basic.page(params[:page]).per(7)
+  end
+
+  def rice
+    @basic = BasicRecipe.where(genre_id:5)
+    @pages = @basic.page(params[:page]).per(7)
+  end
+
+  def soup
+    @basic = BasicRecipe.where(genre_id:6)
+    @pages = @basic.page(params[:page]).per(7)
   end
 
   def show
