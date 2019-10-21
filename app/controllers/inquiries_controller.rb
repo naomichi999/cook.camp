6d4a7fa2139aeedb9170d8179f671006a0dbfc6b
naomichi999 <naomichi999@gmail.com>
2019-10-21 16:15:08 +0900

    [add]お問い合わせ画面の作成

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,8 +1,31 @@
 class InquiriesController < ApplicationController
-	before_action :authenticate_user!
   def new
+  	@inquiry = Inquiry.new
+  	if user_signed_in?
+      @user = User.find(current_user.id)
+    end
   end
 
   def create
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+      flash[:success] = 'お問い合わせありがとうございます。内容が確認次第、管理者からご連絡させていただきます。'
+      if @inquiry.user_id == current_user.id
+        redirect_to users_path
+      else
+      	redirect_to root_path
+      end
+    else
+      flash[:danger] = '送信できませんでした。入力内容をご確認のうえ、再度お問い合わせください。'
+      # ↓renderでnewページを表示させるときにユーザー情報を取得するための記述
+  	  if user_signed_in?
+        @user = User.find(current_user.id)
+      end
+      render :new
+    end
+  end
+    protected
+  def inquiry_params
+    params.require(:inquiry).permit(:user_id, :inquirer_name, :email, :inquiry_title, :inquiry_content)
   end
 end
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,4 +1,4 @@
 class Inquiry < ApplicationRecord
 	has_one :reply, dependent: :destroy
-	validates :inquirer_name, :email,:inquiry_title,:inquiry_content, presence: true
+	validates :inquirer_name, :email, :inquiry_title,:inquiry_content, presence: true
 end
