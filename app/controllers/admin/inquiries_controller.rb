c025186027718f87ba84104ed4fa57dbc33c5b5f
naomichi999 <naomichi999@gmail.com>
2019-10-21 17:43:53 +0900

    [add]お問い合わせ一覧画面の作成(管理者側)

--- app/controllers/admin/inquiries_controller.rb
+++ app/controllers/admin/inquiries_controller.rb
@@ -1,5 +1,7 @@
 class Admin::InquiriesController < ApplicationController
 	before_action :authenticate_admin_user!
   def index
+    @inquiry = Inquiry.all
+    @pages = @inquiry.page(params[:page]).per(7)
   end
 end
