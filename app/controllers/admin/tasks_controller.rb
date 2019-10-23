cfbbfa71c5e4abe061b7e8d7a913d9f2c97ecf61
naomichi999 <naomichi999@gmail.com>
2019-10-23 19:44:46 +0900

    [add]カリキュラムの課題の編集画面の作成(管理者側)

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -9,24 +9,41 @@ class Admin::TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
-      flash[:success] = '新しい節を追加しました。'
+      flash[:success] = '新しい課題を追加しました。'
       redirect_to admin_curriculums_path
     else
-      flash[:danger] = '新しい節は追加できませんでした。'
+      flash[:danger] = '新しい課題は追加できませんでした。'
       redirect_to new_admin_task_path
       # render :show
     end
   end
 
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+    flash[:success] = '課題の情報を編集しました。'
+    redirect_to admin_curriculums_path
+    else
+    flash[:danger] = "課題の情報は編集できませんでした。"
+    redirect_to edit_admin_task_path(@task.id)
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      flash[:success] = '課題を削除しました。'
+      redirect_to admin_curriculums_path
+    else
+      flash[:danger] = "課題の削除に失敗しました。"
+      redirect_to admin_curriculums_path
+    end
   end
   def task_params
     params.require(:task).permit(:section_id, :task_number, :task_title, task_contents_attributes: [:id, :task_image, :task_text, :_destroy])
